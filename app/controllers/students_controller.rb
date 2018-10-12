ee738a81f41efaaede8dc805f44e5aa1a09e4ad1
Jacqueline Chenault <adigitalnative@gmail.com>
2018-10-12 11:34:43 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,25 @@
 class StudentsController < ApplicationController
   
+  before_action :set_student, only: [:show]
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    flash[:notice] = "Student Created"
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def set_student
     @student = Student.find(params[:id])
   end
 
